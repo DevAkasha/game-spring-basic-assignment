0fdd4c48a9e4b2974522a0f8876276346e41aa0b
DevAkasha <hmy9025@gmail.com>
2026-09-09 12:32:47 +0900

    [feat] add card DTO content

--- src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
+++ src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
@@ -1,10 +1,12 @@
 package com.gamebasic.runcard.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 @Getter
 public class RunCardRequest {
-    // TODO (Lv 5): API 명세의 카드 필드 제약을 Bean Validation 어노테이션으로 붙이세요.
+    @NotBlank @Size(min =1)
     private String cardType;
+    @NotNull @Min(0) @Max(10)
     private Integer acquiredFloor;
 }
